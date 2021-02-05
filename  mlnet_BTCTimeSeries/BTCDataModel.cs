89e220d2aff4277a68513e4dbc8967cb48acb1d9
Ted Wagner <ted@Crest-Management.com>
2021-02-05 13:38:37 -0600

    updated data souce, old source was not consistent

---  mlnet_BTCTimeSeries/BTCDataModel.cs
+++  mlnet_BTCTimeSeries/BTCDataModel.cs
@@ -8,8 +8,20 @@ namespace mlnet_BTCTimeSeries
         public int TimeStamp {get;set;}
 
         [LoadColumn(1)]
-        public float Price {get;set;}
+        public float Open {get;set;}
         [LoadColumn(2)]
+        public float High {get;set;}
+        [LoadColumn(3)]
+        public float Low {get;set;}
+        [LoadColumn(4)]
+        public float Close {get;set;}
+
+        [LoadColumn(5)]
+        public float Volume {get;set;}
+        [LoadColumn(6)]
+        public float Currency {get;set;}
+
+        [LoadColumn(7)]
         public float Amount {get;set;}
     }
 }
---  mlnet_BTCTimeSeries/Program.cs
+++  mlnet_BTCTimeSeries/Program.cs
@@ -10,11 +10,15 @@ namespace mlnet_BTCTimeSeries
     class Program
     {
         static MLContext mlContext = new MLContext();
-        static DateTime lastTime;
+        //last time to show the times of our future predictions
+        static DateTime lastTime; 
         static List<BTCDataModel> trainingData;
         static List<BTCDataModel> testingData;
         static string fileName = "btcModel.zip";
-        static int horizon = 5;
+        //how far out we want to predict
+        static int horizon = 5; 
+        //holds or in memory model
+        static ITransformer forecastTransformer; 
         static void Main(string[] args)
         {
             GetTrainingData();
@@ -24,79 +28,71 @@ namespace mlnet_BTCTimeSeries
 
         static void GetTrainingData()
         {
-            IDataView trainingDataFile = mlContext.Data.LoadFromTextFile<BTCDataModel>("btc.csv", hasHeader: false, separatorChar: ',');
+            //load our dataset
+            IDataView trainingDataFile = mlContext.Data.LoadFromTextFile<BTCDataModel>("bitstampUSD_1-min_data_2012-01-01_to_2020-12-31.csv", hasHeader: true, separatorChar: ',');
 
-            //todo: get training and testing data
+            //create enumerable to manipulate data
             List<BTCDataModel> data = mlContext.Data.CreateEnumerable<BTCDataModel>(trainingDataFile, false, true).ToList();
 
             //times in the data set are not uniform, so we will pull unique time values
             data = data.OrderBy( o => o.TimeStamp).ToList();
 
+            //determines the size of our testing data
             int dataSubset = data.Count() - horizon;
-            
 
-            trainingData = data.GetRange(0, dataSubset).ToList(); //create our training data up to the dates we are trying to predict
-            testingData = data.GetRange(dataSubset, horizon);// will get the number of items we are trying to predict
+            //create our training data up to the dates we are trying to predict
+            trainingData = data.GetRange(0, dataSubset).ToList(); 
+
+            // will get the number of items we are trying to predict
+            testingData = data.GetRange(dataSubset, horizon);
             
-            Console.WriteLine(data.Count);
-            
+            //We want to capture time of last item in training data so we can increment the time stamp for our output and put a date/time to the forecast
             lastTime = ConvertTimeStamp(trainingData.Last().TimeStamp);
         }
 
         static void TrainModel()
         {
             IDataView trainingDataView = mlContext.Data.LoadFromEnumerable<BTCDataModel>(trainingData);
-            
 
-            int seriesLength = trainingData.Count()-horizon;
-
+            // creates our estimater, as you cans see we are using forecasting estimator
             var estimator = mlContext.Forecasting.ForecastBySsa(outputColumnName: nameof(PredictedSeriesDataModel.ForecastedPrice),
-                            inputColumnName: nameof(BTCDataModel.Price),
-                            windowSize: 96, //intervals to measure data 96 is equivalent to 24 hours in 15 minute increments giving us 96 datapoints
-                            seriesLength: seriesLength,
-                            trainSize: seriesLength,
+                            inputColumnName: nameof(BTCDataModel.Amount), //column used for time series prediction
+                            windowSize: 60, //series is sampled in 60 minute windows or periods, and the past 60 minutes will be used to make the prediction
+                            seriesLength: 1440, //we want to train over a day's worth of time so this will be the interval, we have 1440 minutes in a day
+                            trainSize: trainingData.Count(), //how many data points we want to sample
                             horizon: horizon,
-                            confidenceLevel: 0.95f,
+                            confidenceLevel: 0.45f, //sets our margin of error, lower the confidence level the smaller the upper/lower bounds
                             confidenceLowerBoundColumn: nameof(PredictedSeriesDataModel.ConfidenceLowerBound),
                             confidenceUpperBoundColumn: nameof(PredictedSeriesDataModel.ConfidenceUpperBound)
             );
 
-            ITransformer forecastTransformer = estimator.Fit(trainingDataView);
-            SaveModel(forecastTransformer, trainingDataView);
-            
+            //creates our fitted model
+            forecastTransformer = estimator.Fit(trainingDataView); 
         }
 
         static void Predict()
         {
-            ITransformer forecastTransformer = LoadModel();
+            //prediction engine based on our fitted model
              TimeSeriesPredictionEngine<BTCDataModel, PredictedSeriesDataModel> forecastEngine = forecastTransformer.CreateTimeSeriesEngine<BTCDataModel, PredictedSeriesDataModel>(mlContext);
 
+            //call to predict the next 5 minutes
              PredictedSeriesDataModel predictions = forecastEngine.Predict();
+
+             //write our predictions
              for(int i = 0; i < predictions.ForecastedPrice.Count(); i++)
-            {
-                Console.WriteLine(ConvertTimeStamp(testingData[i].TimeStamp));
-                lastTime = lastTime.AddMinutes(15);
-                Console.WriteLine("{0} price: {1}, low: {2}, high: {3}, actual: {4}", lastTime, predictions.ForecastedPrice[i].ToString(), predictions.ConfidenceLowerBound[i].ToString(), predictions.ConfidenceUpperBound[i].ToString(), testingData[i].Price);
+            {   
+                lastTime = lastTime.AddMinutes(1);
+                Console.WriteLine("{0} price: {1}, low: {2}, high: {3}, actual: {4}", lastTime, predictions.ForecastedPrice[i].ToString(), predictions.ConfidenceLowerBound[i].ToString(), predictions.ConfidenceUpperBound[i].ToString(), testingData[i].Amount);
             }
 
-            forecastEngine.CheckPoint(mlContext, fileName); //checkpoint updates the model instead of overwriting it. this makes time series training quicker as the data set grows
+
+            //instead of saving, we use checkpoint. This allows us to continue training with updated data and not need to keep such a large data set
+            //so we can append Jan. 2021 without having everythign before to train the model speeding up the process
+            forecastEngine.CheckPoint(mlContext, fileName);
         }
 
-        static void SaveModel(ITransformer Model, IDataView DataView)
-        {
-            mlContext.Model.Save(Model, DataView.Schema, fileName);
-        }
 
-        static ITransformer LoadModel()
-        {
-            DataViewSchema modelSchema;
-            //gets a file from a stream, and loads it
-            using(Stream s = File.Open(fileName, FileMode.Open))
-            {
-                return mlContext.Model.Load(s, out modelSchema);
-            }
-        }
-
+        //helper for converting timestamp to date time
         static DateTime ConvertTimeStamp(double TimeStamp)
         {
             var offset = TimeSpan.FromSeconds(TimeStamp);
